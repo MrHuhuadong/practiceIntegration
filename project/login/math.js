230c8c90e63962d9904a44d2bac3e9dad28c8c45
hu <1947378947@qq.com>
2022-10-18 02:12:26 +0800

    新增用模块化做登录的练习

---
+++ project/login/math.js
@@ -0,0 +1,25 @@
+//导出俩个函数，sum, isOld
+// function sum(a, b) {
+//   return a + b;
+// }
+// function isOld(a) {
+//   return a % 2 == 0;
+// }
+// export default {
+//   sum(a, b) {
+//     return a + b;
+//   },
+//   isOld(a) {
+//     return a % 2 == 0;
+//   },
+// };
+// {
+//   a: 1,
+//   b: 2,
+//   c: function() {},
+//   default: {
+//     a: 1,
+//     b: 2
+//   }
+// }
+export {}
