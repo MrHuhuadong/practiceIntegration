879167f5364b3fa1659349b314fc5a554e9c369f
hu <1947378947@qq.com>
2022-10-18 02:14:53 +0800

    新增用模块化做登录的练习

--- project/login/index.js
+++
@@ -1,9 +0,0 @@
-// import { sum} from "./math.js";
-// import c from "./math.js"
-// console.log(c.sum(7, 9));
-// console.log(c.isOld(6));
-// setTimeout(function () {
-//   import("./math.js").then((r) => {
-//     console.log(r.default.sum(7, 9));
-//   });
-// }, 2000);
